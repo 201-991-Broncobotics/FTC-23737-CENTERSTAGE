e119e75534c5d598e382dc99758fb0214e3dc195
aturico26 <aturico26@brophybroncos.org>
2024-01-26 11:28:57 -0700

    Moving fixed swerve drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/BasicTeleOp.java
@@ -13,6 +13,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.arcrobotics.ftclib.hardware.motors.MotorGroup;
 
+
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 
@@ -34,13 +35,16 @@ public class BasicTeleOp extends LinearOpMode {
             if (gamepad2.dpad_left){
                 robot.DServo.setPosition(.5);
             }
+
             if (gamepad2.left_bumper){
                 robot.LA.setPower(-1);
                 robot.RA.setPower(-1);
-            }
-            if (gamepad2.right_bumper){
+            } else if (gamepad2.right_bumper){
                 robot.LA.setPower(1);
                 robot.RA.setPower(1);
+            } else {
+                robot.LA.setPower(0);
+                robot.RA.setPower(0);
             }
 
 
@@ -48,16 +52,12 @@ public class BasicTeleOp extends LinearOpMode {
                     Math.abs(gamepad1.right_stick_x) * gamepad1.right_stick_x,
                     Math.abs(gamepad1.right_stick_y) * gamepad1.right_stick_y,
                     (Math.abs(gamepad1.left_stick_x) * gamepad1.left_stick_x) * (1 - 0.4 * gamepad1.left_trigger),
-                    1 - 0.6 * gamepad1.left_trigger);
+                    1 - 0.6 * gamepad1.left_trigger * (1 - gamepad1.right_trigger)); // right trigger stops motors but still lets servos spin
 
 
             telemetry.addData("FPS:", Math.round((1 / (mRuntime.time() - LastTime)) * 1000));
             LastTime = mRuntime.time();
-            telemetry.addData("RF:", robot.RF.getCurrentPosition());
-            telemetry.addData("LF:", robot.LF.getCurrentPosition());
-            telemetry.addData("LB:", robot.LB.getCurrentPosition());
-            telemetry.addData("RB:", robot.RB.getCurrentPosition());
-            telemetry.update();
+            //telemetry.update(); // telemetry is temporary updating in swerve drive method to make sure telemetry shows up
         }
     }
 }
