9c4b7124f835f3673d39ab008dd49b240c295408
aturico26 <aturico26@brophybroncos.org>
2024-01-26 12:30:06 -0700

    Swerve Works!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/BasicTeleOp.java
@@ -1,16 +1,18 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.teleop;
 
 
+//import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
+//import com.arcrobotics.ftclib.hardware.motors.MotorGroup;
+
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
@@ -19,7 +21,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class BasicTeleOp extends LinearOpMode {
 
     public void runOpMode() throws InterruptedException {
-        RobotHardware robot = new RobotHardware(hardwareMap, telemetry);
+        org.firstinspires.ftc.teamcode.teleop.RobotHardware robot = new org.firstinspires.ftc.teamcode.teleop.RobotHardware(hardwareMap, telemetry);
 
         Gamepad driver = gamepad1, operator = gamepad2;
 
@@ -28,25 +30,35 @@ public class BasicTeleOp extends LinearOpMode {
 
         waitForStart();
 
-
-
         while (opModeIsActive() && !isStopRequested()) {
 
+            if (gamepad2.dpad_left){
+                robot.DServo.setPosition(.5);
+            }
+
+            if (gamepad2.left_bumper){
+                robot.LA.setPower(-1);
+                robot.RA.setPower(-1);
+            } else if (gamepad2.right_bumper){
+                robot.LA.setPower(1);
+                robot.RA.setPower(1);
+            } else {
+                robot.LA.setPower(0);
+                robot.RA.setPower(0);
+            }
 
 
-            robot.driveSwerveWithControllers(Math.abs(gamepad1.right_stick_x) * gamepad1.right_stick_x,
+            robot.driveSwerveWithControllers(
+                    Math.abs(gamepad1.right_stick_x) * gamepad1.right_stick_x,
                     Math.abs(gamepad1.right_stick_y) * gamepad1.right_stick_y,
                     (Math.abs(gamepad1.left_stick_x) * gamepad1.left_stick_x) * (1 - 0.4 * gamepad1.left_trigger),
-                    1 - 0.6 * gamepad1.left_trigger);
+                    (1 - 0.6 * gamepad1.left_trigger) * (1 - gamepad1.right_trigger)); // right trigger stops motors but still lets servos spin
 
 
             telemetry.addData("FPS:", Math.round((1 / (mRuntime.time() - LastTime)) * 1000));
+            telemetry.addData("Throttle:", 1 - 0.6 * gamepad1.left_trigger * (1 - gamepad1.right_trigger));
             LastTime = mRuntime.time();
-            telemetry.addData("RF:", robot.RF.getCurrentPosition());
-            telemetry.addData("LF:", robot.LF.getCurrentPosition());
-            telemetry.addData("LB:", robot.LB.getCurrentPosition());
-            telemetry.addData("RB:", robot.RB.getCurrentPosition());
-            telemetry.update();
+            //telemetry.update(); // telemetry is temporary updating in swerve drive method to make sure telemetry shows up
         }
     }
 }
